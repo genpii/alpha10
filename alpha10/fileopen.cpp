634168c94c53786e52229a2a0c321212d0984d9f
Gen Onodera <genpiichaan@gmail.com>
2015-10-06 20:51:08 +0900

    1006

---
+++ alpha10/fileopen.cpp
@@ -0,0 +1,10 @@
+// fileopen.cpp
+
+#include "stdafx.h"
+#include <iostream>
+#include <iosfwd>
+
+void open()
+{
+	ifstream ifs("source", ios_base::binary);
+}
---
+++ alpha10/main.cpp
@@ -0,0 +1,11 @@
+// main.cpp ƒƒCƒ“
+// Gen Onodera
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
