f8d04d62c58deffb22d0d1c799a0b75fca1f013d
Gen Onodera <genpiichaan@gmail.com>
2015-10-08 20:24:23 +0900

    1008

--- alpha10/fileopen.cpp
+++ alpha10/fileopen.cpp
@@ -4,15 +4,5 @@
 #include <fstream>
 #include "fileopen.h"
 
-using namespace std;
+//using namespace std;
 
-int fileopen()
-{
-	ifstream ifs("./RF/sample20151005_2.crf", ios_base::binary);
-	if (!ifs){
-		cout << "could't open\n";
-		return 1;
-	}
-	
-	
-}
---
+++ alpha10/fileopen.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class RF {
+	int in_i;
+	float in_f;
+	double in_d;
+	unsigned short in_s;
+	char *in_c;
+public:
+	void fileopen();
+
+	
+
+};
